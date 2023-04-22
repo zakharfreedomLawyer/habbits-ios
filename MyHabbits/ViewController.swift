9c25ee2ef7736b029b502213b158d8177096d7e7
zakharfreedomLawyer <107808581+zakharfreedomLawyer@users.noreply.github.com>
2023-04-22 12:53:39 +0300

    Initial Commit

---
+++ MyHabbits/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyHabbits
+//
+//  Created by Захар Кисляк on 22.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
